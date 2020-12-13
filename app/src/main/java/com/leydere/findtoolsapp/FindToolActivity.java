f982279519cce996a95f3b12eddd76e7b7ab1752
Eric Leyder <47430289+leydere@users.noreply.github.com>
2020-12-13 15:46:09 -0500

    Set navigation using bottom nav
    Added two new activities.  Navigation using bottom nav bar is functional.  Minor aesthetic issues.

--- app/src/main/java/com/leydere/findtoolsapp/FindToolActivity.java
+++ app/src/main/java/com/leydere/findtoolsapp/FindToolActivity.java
@@ -1,32 +1,33 @@
 package com.leydere.findtoolsapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class FindToolActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_find_tool);
 
-        TextView title = (TextView) findViewById(R.id.activityMainTitle);
-        title.setText("This is the Home activity.");
+        TextView title = (TextView) findViewById(R.id.activityFindTitle);
+        title.setText("This is the Find Tool activity.");
 
         //code to support override of bottom nav animation
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
         //supports correct icon being highlighted
         Menu menu = bottomNavigationView.getMenu();
-        MenuItem menuItem = menu.getItem(0);
+        MenuItem menuItem = menu.getItem(1);
         menuItem.setChecked(true);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -35,15 +36,15 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()){
 
                     case R.id.ic_home:
+                        Intent intentHome = new Intent(FindToolActivity.this, MainActivity.class);
+                        startActivity(intentHome);
                         break;
 
                     case R.id.ic_search:
-                        Intent intentFind = new Intent(MainActivity.this, FindToolActivity.class);
-                        startActivity(intentFind);
                         break;
 
                     case R.id.ic_add:
-                        Intent intentAdd = new Intent(MainActivity.this, AddToolActivity.class);
+                        Intent intentAdd = new Intent(FindToolActivity.this, AddToolActivity.class);
                         startActivity(intentAdd);
                         break;
                 }
