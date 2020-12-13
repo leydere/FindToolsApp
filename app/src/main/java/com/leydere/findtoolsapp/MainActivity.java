d334365c427407c6a1c00a92753cdb6c955efb13
Eric Leyder <47430289+leydere@users.noreply.github.com>
2020-12-13 14:42:23 -0500

    Refactored bottom nav bar
    Replaced home brew bottom nav bar with bottom nav bar widget.  Overrode animation with new helper class.  Seated layout features within nested relative layouts.

--- app/src/main/java/com/leydere/findtoolsapp/MainActivity.java
+++ app/src/main/java/com/leydere/findtoolsapp/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //code to support override of bottom nav animation
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
     }
 }
